bbf17e60f6667bbe775f05e2db617cf62218db16
Layered <68783120+Layeredplayz@users.noreply.github.com>
2023-04-20 20:36:24 -0400

    Added information regarding Pterodactyl Allocation and Pterodactyl Skip Scripts for admins

--- Servers/Pterodactyl/index.php
+++ Servers/Pterodactyl/index.php
@@ -104,12 +104,14 @@ function Pterodactyl_getProductConfig()
             'name' => 'skip_scripts',
             'friendlyName' => 'Pterodactyl Skip Scripts',
             'type' => 'boolean',
+            'description' => 'Decides if Pterodactyl will skip install scripts',
         ],
         [
             'name' => 'allocation',
             'friendlyName' => 'Pterodactyl Allocation',
             'type' => 'text',
             'required' => true,
+            'description' => 'How many ports the user can allocate. Usually, 1 will work',
         ],
     ];
 }
