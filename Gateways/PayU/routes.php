7c6cdf4172e4522814d76b944ecdd781ffee7f96
Corwin <info@corwindev.nl>
2023-06-28 18:19:43 +0200

    fix: fixed typo

--- Gateways/PayU/routes.php
+++ Gateways/PayU/routes.php
@@ -5,9 +5,9 @@ use Illuminate\Support\Facades\Route;
 include_once __DIR__ . '/index.php';
 
 Route::post('/payu/cancel', function () {
-    return UPay_cancel(request());
+    return PayU_cancel(request());
 })->name('payu.cancel');
 
 Route::post('/payu/success', function () {
-    return UPay_success(request());
+    return PayU_success(request());
 })->name('payu.success');
