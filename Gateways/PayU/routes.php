06240bc236544fa6745c2da99bb12859697bde94
Corwin <info@corwindev.nl>
2023-09-26 17:26:51 +0100

    fix

--- Gateways/PayU/routes.php
+++ Gateways/PayU/routes.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-include_once __DIR__ . '/index.php';
-
 Route::post('/payu/cancel', [\App\Extensions\Gateways\PayU\PayU::class, 'cancel'])->name('payu.cancel');
 
 Route::post('/payu/success', [\App\Extensions\Gateways\PayU\PayU::class, 'success'])->name('payu.success');
